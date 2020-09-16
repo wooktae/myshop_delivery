7b8ee3a64de0073342e8cee60d51a954f8d300ec
wooktae1 <yukiever@naver.com>
2020-09-16 11:03:47 +0900

    first

---
+++ src/main/java/myShop/CancellationRepository.java
@@ -0,0 +1,8 @@
+package myShop;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CancellationRepository extends PagingAndSortingRepository<Cancellation, Long>{
+
+
+}
---
+++ src/main/java/myShop/DeliveryRepository.java
@@ -0,0 +1,8 @@
+package myShop;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface DeliveryRepository extends PagingAndSortingRepository<Delivery, Long>{
+
+
+}
